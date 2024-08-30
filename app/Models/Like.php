9b378da73874e269e48e7bfc989e1d1b60f4ca17
letrongkhaimedi2607 <139963447+letrongkhaimedi2607@users.noreply.github.com>
2024-08-30 18:11:42 +0700

    Add like model

---
+++ app/Models/Like.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'thread_id'];
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
